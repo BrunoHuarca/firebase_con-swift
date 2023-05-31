e40d52fac8a9ea7f94ec4357042f232ad4fbea59
felipefm12 <jorge.flores.m@tecsup.edu.pe>
2023-05-31 08:21:51 -0500

    Initial Commit

---
+++ HuarcaSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuarcaSnapchat
+//
+//  Created by Mac 06 on 31/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
